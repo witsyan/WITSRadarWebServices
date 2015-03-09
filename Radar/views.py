f417d682ce5bf56d9a281b25e2e1a858d4258645
WitsTools <admin@y.local>
2015-03-09 14:55:55 +0800

    views.py 去除多余代码
    yan 3/09

--- Radar/views.py
+++ Radar/views.py
@@ -43,163 +43,7 @@ webinfo = {
     'welcomecontent':'Select the projects in the left side to view radar detials.',
     'noprojecttitle':'Error',
     'noprojectcontent':'Can\'t find the project: ',
-    }
-def getweekslist2(s = "2015-2-8", e = "2015-03-18"):
-    if s > e:
-        s, e = e, s
-    #print calendar.monthcalendar(2015,2)
-    sdt = datetime.strptime(s, '%Y-%m-%d')#2014-12-31
-    edt = datetime.strptime(e, '%Y-%m-%d')#2015-12-31
-    syear = int(sdt.strftime('%Y'))#取出开始年
-    smonth = int(sdt.strftime('%m'))#取出开始月
-    sday = int(sdt.strftime('%d'))#取出开始日
-    
-    eyear = int(edt.strftime('%Y'))#取出结束年
-    emonth = int(edt.strftime('%m'))#取出结束月
-    eday = int(edt.strftime('%d'))#取出结束日
-    
-    sweek = ""#星期开始日期
-    sw = calendar.weekday(syear, smonth, sday)
-    print 'bbbb', sw, syear, smonth, sday
-    if sw != 0:
-        star = sdt + timedelta(days = -sw)
-        sweek = star.strftime('%Y-%m-%d')
-        print 'ssssweek', sweek, sw
-    else:
-        star = sdt
-        sweek = sdt.strftime('%Y-%m-%d')
-        print 'xxxxsweek', sweek, sw
-    print 'sweek', sweek, sw
-    #星期结束日期
-    eweek = ""
-    sw = calendar.weekday(eyear, emonth, eday)
-    if sw < 6:
-        end = edt + timedelta(days = 6 - sw)
-        eweek = end.strftime('%Y-%m-%d')
-    else:
-        end = edt
-        eweek = edt.strftime('%Y-%m-%d')
-    print 'eweek', eweek, sw
-    
-    www = []
-    sstar = star
-    eend = star + timedelta(days = 6)
-    if end == eend:
-        www.append('%s~%s'%(star.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')))
-    i = 0
-    while end != eend:
-        i = i + 1
-     #   print ""
-        www.append('%s~%s'%(sstar.strftime('%Y-%m-%d'), eend.strftime('%Y-%m-%d')))
-        print '%s~%s'%(sstar.strftime('%Y-%m-%d'), eend.strftime('%Y-%m-%d')), end, eend, i
-        sstar = eend + timedelta(days = 1)
-        eend = sstar + timedelta(days = 6)
-        if end == eend:
-            www.append('%s~%s'%(sstar.strftime('%Y-%m-%d'), eend.strftime('%Y-%m-%d')))
-            
-
-    print www, sstar, eend
-    return www    
-
-
-
-
-def getweekslist(s = "2015-1-15", e = "2016-3-22"):
-    if s > e:
-        s, e = e, s
-    #calendar.monthcalendar(2015,1)
-    sdt = datetime.strptime(s, '%Y-%m-%d')#2014-12-31
-    edt = datetime.strptime(e, '%Y-%m-%d')#2015-12-31
-    syear = int(sdt.strftime('%Y'))#取出开始年
-    smonth = int(sdt.strftime('%m'))#取出开始月
-    sday = int(sdt.strftime('%d'))#取出开始日
-
-    eyear = int(edt.strftime('%Y'))#取出结束年
-    emonth = int(edt.strftime('%m'))#取出结束月
-    eday = int(edt.strftime('%d'))#取出结束日
-    
-    sweek = ""#星期开始日期
-    sw = calendar.weekday(syear, smonth, sday)
-    if sw != 0:
-        ed = sdt + timedelta(days = -sw)
-        sweek = ed.strftime('%Y-%m-%d')
-    else:
-        sweek = sdt.strftime('%Y-%m-%d')
-    print 'sweek', sweek
-    #星期结束日期
-    eweek = ""
-    sw = calendar.weekday(eyear, emonth, eday)
-    if sw < 6:
-        ed = edt + timedelta(days = 6 - sw)
-        eweek = ed.strftime('%Y-%m-%d')
-    else:
-        eweek = edt.strftime('%Y-%m-%d')
-    print 'eweek', eweek, sw
-
-    
-    yearlist = range(syear, eyear + 1)#年列表
-    print 'yearlist', yearlist
-    weekslist = []#星期列表
-    monthslist = []#取得月列表
-    for y in range(len(yearlist)):
-        if yearlist[y] == syear and yearlist[y] == eyear:
-            monthslist.append(range(smonth, emonth + 1))#月列表
-        elif yearlist[y] == syear:
-            monthslist.append(range(smonth, 12 + 1))#月列表
-        elif yearlist[y] == eyear:
-            monthslist.append(range(1, emonth + 1))#月列表
-        else:
-            monthslist.append(range(1, 13))#月列表
-    print 'monthslist', monthslist
-        
-    for y in range(len(yearlist)):
-        mlist = monthslist[y]#月列表
-        for m in mlist:
-            wlist = calendar.monthcalendar(yearlist[y], m)
-            for w in wlist:
-                if int(w[0]) != 0:
-                    wd = datetime.strptime("%s-%s-%s"%(yearlist[y], m, w[0]), '%Y-%m-%d')
-                    sss = wd.strftime('%Y-%m-%d')
-                    if sss not in weekslist:
-                        weekslist.append(sss)
-                        #print '0', wd.strftime('%Y-%m-%d')
-                else:
-                    for i in range(len(w)):
-                        if int(w[i]) > 0:
-                            eee = datetime.strptime('%s-%s-%s'%(yearlist[y], m, w[i]), '%Y-%m-%d') + timedelta(days = -i)
-                            sss = eee.strftime('%Y-%m-%d')
-                            if sss not in weekslist:
-                                weekslist.append(sss)
-                                print 'SSSSSSSSS', sss
-                            break
-                if int(w[-1]) != 0:
-                    wd = datetime.strptime("%s-%s-%s"%(yearlist[y], m, w[-1]), '%Y-%m-%d')
-                    sss = wd.strftime('%Y-%m-%d')
-                    if sss not in weekslist:
-                        weekslist.append(sss)
-                        print '-1', wd.strftime('%Y-%m-%d')
-                else:
-                    for i in range(len(w)):
-                        if int(w[i]) == 0:
-                            ddd = datetime.strptime('%s-%s-%s'%(yearlist[y], m, w[i-1]), '%Y-%m-%d') + timedelta(days = 7 - i)
-                            sss = ddd.strftime('%Y-%m-%d')
-                            if sss not in weekslist:
-                                weekslist.append(sss)
-                                print 'XXXXXXX', sss
-                            break            
-    sw = 0
-    ew = 0
-    print weekslist
-    for w in range(len(weekslist)):
-        if weekslist[w] == sweek:
-            sw = w
-        if weekslist[w] == eweek:
-            ew = w
-    weekslist = weekslist[sw:ew + 1]   
-    print weekslist, sweek, eweek
-    return weekslist
-    
-    
+    }  
 
 def RadarWebServiceskeywords(request):
     #获取数据
@@ -209,11 +53,9 @@ def RadarWebServiceskeywords(request):
     dateformto = ""
     startweek = request.GET.get('startweek','')
     endweek = request.GET.get('endweek','')
-    print startweek, endweek
     if(startweek.split("~")[0] > endweek.split("~")[0]):
         #print startday, endday
         startweek, endweek = endweek, startweek
-    print startweek, endweek
     if startweek != '' and endweek != '':
         dateformto = "%s~%s"%(startweek.split("~")[0], endweek.split("~")[-1])
     if dateformto != "":
@@ -221,14 +63,9 @@ def RadarWebServiceskeywords(request):
     else:
         kcolumnbasic,radarurls=getCountAndDetailFromAssignee(project)
     weeksList = getWeekList(project)
-    #print radarurls
     x, y = getMaxAndMinDayFromAssignee(project,table="WitsAssignee")
-    print "xxx", x, y
     xx = getweekslist2(x,y)
     ii = getweekslist(x,y)
-    print "VvvvVVV", xx
-    print "VvvvVVV", ii
-    #print kcolumnbasic
     return render_to_response('RadarWebServiceskeywords.html',{"project":project, "dailys":dailys, 'webinfo':webinfo, 'kcolumnbasic':kcolumnbasic, 'radarurls':radarurls, 'weeksList':weeksList, 'startweek':startweek, 'endweek':endweek,})
 
 
@@ -316,7 +153,5 @@ def RadarWebServicesprojectselect(request):
     #获取数据
     columnbasic,usbugdetail,projectdetials,projectdaily = getCountAndDetailFromDaily(project,startday,endday)
     containerline = getCountFromCount(project)
-    #print projectdetials['newlist']
-    #print 'RadarWebServicesprojectselect', project
     return render_to_response('RadarWebServicesproject.html',{"projects":projects, "project":project, "dailys":dailys, 'webinfo':webinfo, 'startday':startday, 'endday':endday, 'columnbasic':columnbasic ,'containerline':containerline, 'projectdetials':projectdetials, 'projectdaily':projectdaily, 'usbugdetail':usbugdetail })
 
