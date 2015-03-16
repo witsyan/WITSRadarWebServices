6931ee02bb94f75d9e7c6ef757b4de9120c528fe
WitsTools <admin@y.local>
2015-03-16 22:57:40 +0800

    3.16

--- ScheduleTest/views.py
+++ ScheduleTest/views.py
@@ -18,11 +18,63 @@ from datetime import *
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-def ScheduleTest(request):
+#网页信息
+webinfo = {
+    'title':'TSTT',
+    'copyright':'Copyright Wistronits',
+    'base':'Data Based on Radar API.',
+    'welcometitle':'Welcome',
+    'welcomecontent':'请单击选择要显示的内容！',
+    'noprojecttitle':'Error',
+    'noprojectcontent':'Can\'t find the project: ',
+    }
+
+def sssScheduleTest(request):
     select_weeks = 0
+    request.session['select_weeks'] = select_weeks
     projects = projList(weekly=select_weeks)
     weekly = weekToDate(select_weeks)
-    return render_to_response('ScheduleTest.html',{"projects":projects, "CHeight":getHseight(projects), "BHeight":getHseight(projects) + 4, "weekly":weekly},)
+    print weekly
+    print projects
+    return render_to_response('ScheduleTest.html',{"projects":projects, "CHeight":getHseight(projects), "BHeight":getHseight(projects) + 4, "weekly":weekly, "webinfo":webinfo},)
+
+def ScheduleTest(request):
+    seleckweekly = request.GET.get('seleckweekly','0')
+    if('select_weeks' in request.session):
+        select_weeks = request.session['select_weeks']
+    else:
+        select_weeks = 0
+    if seleckweekly == '0':
+        select_weeks = 0
+    else:
+        select_weeks = int(select_weeks) + int(seleckweekly)
+    request.session['select_weeks'] = select_weeks
+    projects = projList(weekly=select_weeks)
+    weekly = weekToDate(select_weeks)
+    #print weekly
+    #print projects
+    print 'ScheduleTest select_weeks', select_weeks
+    return render_to_response('ScheduleTest.html',{"projects":projects, "CHeight":getHseight(projects), "BHeight":getHseight(projects) + 4, "weekly":weekly, "webinfo":webinfo, "select_weeks":select_weeks},)
+
+
+def ScheduleTestselect(request):
+    return render_to_response('ScheduleTestwelcome.html',{ "webinfo":webinfo},)
+
+def ScheduleTestseleckweekly(request):
+    seleckweekly = request.GET.get('seleckweekly','0')
+    if('select_weeks' in request.session):
+        select_weeks = request.session['select_weeks']
+    else:
+        select_weeks = 0
+    if seleckweekly == '0':
+        select_weeks = 0
+    else:
+        select_weeks = int(select_weeks) + int(seleckweekly)
+    request.session['select_weeks'] = select_weeks
+    projects = projList(weekly=select_weeks)
+    weekly = weekToDate(select_weeks)
+    print 'ScheduleTestseleckweekly select_weeks', select_weeks
+    return render_to_response('ScheduleTestDetail.html',{"projects":projects, "CHeight":getHseight(projects), "BHeight":getHseight(projects) + 4, "weekly":weekly, "webinfo":webinfo},)
 
 #参照 ScheduleTest.js function getHseight()
 def getHseight(ps):
